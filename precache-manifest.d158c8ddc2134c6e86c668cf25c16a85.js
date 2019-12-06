0245a07b2f0e3e47664069088757bd211703cb50
bemerson <bemerson@gracehill.com>
2019-12-05 20:30:44 -0500

    Updates

---
+++ precache-manifest.d158c8ddc2134c6e86c668cf25c16a85.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13405dc8a30e34b17db016d3c8149e8",
+    "url": "/ox-website/index.html"
+  },
+  {
+    "revision": "29125c442cfd493184f0",
+    "url": "/ox-website/static/css/main.ea45b1fc.chunk.css"
+  },
+  {
+    "revision": "13060d0bce1b2abb884e",
+    "url": "/ox-website/static/js/2.6aa9449b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ox-website/static/js/2.6aa9449b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29125c442cfd493184f0",
+    "url": "/ox-website/static/js/main.0b5c3538.chunk.js"
+  },
+  {
+    "revision": "7e15d78e2be9f1269dfd",
+    "url": "/ox-website/static/js/runtime-main.f834a976.js"
+  },
+  {
+    "revision": "9d1ebf9ad495dd442be39dbe5b2dcf47",
+    "url": "/ox-website/static/media/OXB-FULL.9d1ebf9a.png"
+  },
+  {
+    "revision": "3339b84fa7f210b5c02e47f9186b3f9e",
+    "url": "/ox-website/static/media/OXB-sticker.3339b84f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ox-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
